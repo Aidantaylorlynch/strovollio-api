e720dd7f3ee026ca54166fb0c344c0a7b81f1e01
nativestackau <aidan@nativestack.com.au>
2020-02-19 11:41:45 +1100

    revert

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,7 @@ namespace strovollio_api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors(b => b.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+            app.UseCors(builder => builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod());
 
             app.UseHttpsRedirection();
 
